eb387e59e668511b1dad9521eb36c981a086ab30
Chinmay <patilchinmay01@gmail.com>
2019-04-24 23:19:01 +0530

    Login controller function created

--- application/components/users/user_controller.js
+++ application/components/users/user_controller.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const async = require("async");
 const response_service = require('../../utils/response_service');
+const config = require('../../../config/index')
 
 function hash_password(password){
     return new Promise(async (resolve, reject) => {
@@ -19,7 +20,6 @@ function hash_password(password){
                     }, res);
                     
                 }else{
-                    console.log(`password_hash_ = ${hash}`)
                     resolve(hash); 
                 }
             });
@@ -57,6 +57,7 @@ function check_unique_user(email){
     })
 }
 
+// Register a user
 exports.user_register = async (req, res, next) => {
     const email     = req.body.email || '';
     const password  = req.body.password || '';
@@ -81,9 +82,6 @@ exports.user_register = async (req, res, next) => {
             password: password_hash,
             timestamp: Date.now()
         });
-
-        console.log(user);
-        console.log(`password_hash = ${password_hash}`);
         
         // Create user in the mongo db.
         user.save()
@@ -104,7 +102,84 @@ exports.user_register = async (req, res, next) => {
     }else{
         response_service.send({
             status: response_service.getCode().codes.BAD_REQUEST,
-            data: 'Please provide username and password',
+            message: 'Please provide email and password',
+        }, res);
+    }
+}
+
+// Log in a user
+exports.user_login = async (req, res, next) => {
+    const email     = req.body.email || '';
+    const password  = req.body.password || '';
+
+    if (email && password) {
+        User.find({email:email})
+            .select()
+            .exec()
+            .then(user => {
+                // More thatn 1 user found with the same email
+                if(user.length < 1){
+                    return response_service.send({
+                        status: response_service.getCode().codes.FAILURE,
+                        message: 'Authentication Failed!',
+                    }, res);
+                }
+
+                // Validate password
+                bcryptjs.compare(password, user[0].password, (err, result) => {
+                    if(err){
+                        // If there is an error while comparing. System couldn't compare passwords.
+                        return response_service.send({
+                            status: response_service.getCode().codes.FAILURE,
+                            message: 'Authentication Failed!',
+                        }, res);
+                    } else if (result){
+                        //If the password is correct. Create a token for sending it to user
+                        const token = jwt.sign({
+                            email: user[0].email,
+                            user_id: user[0]._id
+                        },
+                        config.JWT_KEY,         // SERVE THROUGH ENVIRONMENT VARIABLE IN PRODUCTION
+                        {
+                            expiresIn: "24h" // Update var expires
+                        });
+
+                        var signed = Date.now();
+                        var expires = signed + (24*60*60*1000); //(Hours*Minutes*Seconds*Miliseconds)
+                        
+                        // Send the token
+                        return response_service.send({
+                            status: response_service.getCode().codes.OK,
+                            message: 'Authentication Successful!',
+                            data: {
+                                    token: token,
+                                    _id: user[0]._id,
+                                    iat: signed,
+                                    exp: expires,
+                                    email: user[0].email
+                            }
+                        }, res);
+                        
+                    } else {
+                        // If the password is incorrect
+                        return response_service.send({
+                            status: response_service.getCode().codes.FAILURE,
+                            message: 'Authentication Failed!',
+                        }, res);
+                    }
+                });
+            })
+            .catch(error => {
+                return response_service.send({
+                    status: response_service.getCode().codes.FAILURE,
+                    message: 'An error occured!',
+                    data: err
+                }, res);
+            });
+    }else{
+        response_service.send({
+            status: response_service.getCode().codes.BAD_REQUEST,
+            data: 'Please provide email and password',
         }, res);
     }
 }
