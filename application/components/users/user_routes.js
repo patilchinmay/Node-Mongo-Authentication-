a09da209a5fa34ba28a6ee1a2919be0a4edab50e
Chinmay <patilchinmay01@gmail.com>
2019-04-25 08:07:36 +0530

    Added route for protected logged in route

--- application/components/users/user_routes.js
+++ application/components/users/user_routes.js
@@ -1,14 +1,7 @@
 const express = require('express');
-
 const router = express.Router();
-
 const checkAuth = require('../../middlewares/check-auth');
-
-const User = require('./user_model');
 const UserController = require('./user_controller');
-
-// const checkVersion = require('../middleware/check_version');
-
 
 // Add a new user
 
@@ -17,5 +10,9 @@ router.post('/register', UserController.user_register);
 // Create token for a user and log him in
 
 router.post('/login', UserController.user_login);
+
+// Create a protected route. It requires user to be logged in to access.
+
+router.post('/protected', checkAuth, UserController.user_protected);
 
 module.exports = router;
